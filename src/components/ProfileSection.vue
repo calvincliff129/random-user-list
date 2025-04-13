cc67b058f560eb104329011a1767756468fa10ab
calvin <calvin.cliff@impact-multimedia.com>
2025-04-13 14:35:01 +0800

    feat: adds responsiveness to the sections on various screen sizes

--- src/components/ProfileSection.vue
+++ src/components/ProfileSection.vue
@@ -7,13 +7,13 @@ const onlineStatus = ref('Last online: 2 days ago')
 </script>
 
 <template>
-    <div class="relative flex justify-center items-end bg-[#4EBBD8] gap-2 py-2 h-36 text-white">
+    <div class="relative flex justify-center items-end bg-[#4EBBD8] gap-2 py-4 md:py-2 min-h-36 h-fit text-white">
         <div
-            class="flex flex-row items-end justify-start sm:flex-row sm:items-start gap-4 w-full max-w-7xl h-full -mb-8">
+            class="flex flex-col md:flex-row items-center md:items-center justify-start gap-4 w-full max-w-7xl h-full md:-mb-8">
             <img alt="Kiratech logo" class="rounded w-32 h-32" src="@/assets/Avatar.png" />
 
-            <div class="flex flex-row justify-end items-center gap-6 h-full">
-                <div class="flex flex-col">
+            <div class="flex flex-col md:flex-row justify-end items-center gap-6 h-full">
+                <div class="flex flex-col text-center md:text-start">
                     <h2 class="text-3xl font-bold">{{ userName }}</h2>
                     <p class="text-xs font-light">{{ onlineStatus }}</p>
                 </div>
