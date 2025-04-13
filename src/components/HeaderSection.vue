5682dd03d57e7bc4b8134a7cccb5232a8d1a59ad
calvin <calvin.cliff@impact-multimedia.com>
2025-04-13 13:50:27 +0800

    feat: develops the header component consisting of the logo and nav items

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BellIcon from '@/components/icons/BellIcon.vue'
+import GearIcon from '@/components/icons/GearIcon.vue'
+import LogOutIcon from '@/components/icons/LogOutIcon.vue'
+</script>
+
+<template>
+    <div class="flex flex-row justify-between items-center max-w-7xl w-full h-12 mx-auto">
+        <!-- Logo (scaled to fit header height) -->
+        <img alt="Kiratech logo" src="@/assets/Logo.png" class="h-8 w-auto" />
+
+        <!-- Icons -->
+        <div class="flex flex-row gap-4">
+            <BellIcon class="cursor-pointer hover:scale-110 transition-transform" />
+            <GearIcon class="cursor-pointer hover:scale-110 transition-transform" />
+            <LogOutIcon class="cursor-pointer hover:scale-110 transition-transform" />
+        </div>
+    </div>
+</template>
